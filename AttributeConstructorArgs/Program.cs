9c78766031af65f73f55fc995f938798723290a1
Martin Zikmund <martin@zikmund.dev>
2022-11-15 12:00:26 +0100

    Attribute constructor args sample

---
+++ AttributeConstructorArgs/Program.cs
@@ -0,0 +1,13 @@
+﻿var attributeData = typeof(TestClass).GetCustomAttributesData()[0];
+Console.WriteLine(attributeData.AttributeType == typeof(TestAttribute)); // true
+Console.WriteLine(attributeData.ConstructorArguments[0].Value); // "Hello World"
+
+[Test("Hello, world!")]
+public class TestClass
+{    
+}
+
+public class TestAttribute : Attribute
+{
+    public TestAttribute(string greeting) { }
+}
